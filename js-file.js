dbf63187e809012b0b442fe23b46494751dc99a5
ghostnoodles <35480737+ghostnoodles@users.noreply.github.com>
2024-01-27 20:16:41 +1300

    playing with DOM manipulation

---
+++ js-file.js
@@ -0,0 +1,28 @@
+const redId = document.querySelector('#red');
+
+const red = document.createElement('p');
+red.textContent = 'Hey I’m red!';
+red.style.cssText = 'color: red';   
+
+redId.appendChild(red);
+
+const blueId = document.querySelector('#blue');
+
+const blue = document.createElement('h3');
+blue.textContent = 'I’m a blue h3!';
+blue.style.cssText = 'color: blue';   
+
+blueId.appendChild(blue);
+
+const divId = document.querySelector('#div');
+divId.style.cssText = 'background-color: pink;border: solid black';  
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I’m in a div';
+divId.appendChild(h1);
+
+const p = document.createElement('p');
+p.textContent = 'ME TOO!';
+divId.appendChild(p);
+
+<button onclick="alert('Hello World')">Click Me</button>
